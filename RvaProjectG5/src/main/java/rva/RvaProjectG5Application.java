d094b1585079befd113364db118bea2330918beb
Vladimir Fabri <vfficial98@gmail.com>
2022-04-18 13:20:20 +0200

    Dodatak kontrolera - DobavljacController i PorudzbinaController

--- RvaProjectG5/src/main/java/rva/RvaProjectG5Application.java
+++ RvaProjectG5/src/main/java/rva/RvaProjectG5Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RvaProjectG4Application {
+public class RvaProjectG5Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RvaProjectG4Application.class, args);
+		SpringApplication.run(RvaProjectG5Application.class, args);
 	}
 
 }
